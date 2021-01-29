ff60958d655f2a53691a5703dc3f86113ce0e26f
Clement <clementbonet@hotmail.fr>
2021-01-29 16:23:42 +0100

    update training_mnist

--- Normalizing Flows/utils/NF_base.py
+++ Normalizing Flows/utils/NF_base.py
@@ -12,135 +12,186 @@ from torch.distributions import Uniform, TransformedDistribution, SigmoidTransfo
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-class BaseNormalizingFlow(ABC,nn.Module):
+
+class BaseNormalizingFlow(ABC, nn.Module):
     def __init__(self):
         super().__init__()
 
     @abstractmethod
-    def forward(self,x):
+    def forward(self, x):
         pass
 
     @abstractmethod
-    def backward(self,z):
+    def backward(self, z):
         pass
 
 
 class AdditiveCoupling(BaseNormalizingFlow):
     """
-        NICE
+    	Additive coupling layer
+
+    	Refs:
+    	- NICE: Non-linear independent components estimation
     """
-    def __init__(self, coupling, d):
+
+    def __init__(self, coupling, dim):
         super().__init__()
-        self.k = d//2
+        self.k = dim//2
         self.coupling = coupling
-        
+
     def forward(self, x):
-        x0, x1 = x[:,:self.k], x[:,self.k:]
+    	"""
+    		Outputs:
+    		- output coupling layer
+    		- log determinant of the Jacobian
+    	"""
+        x0, x1 = x[:, :self.k], x[:, self.k:]
         
         m = self.coupling(x0)
         z0 = x0
         z1 = x1+m
-            
-        z = torch.cat([z0,z1], dim=1)
-        return z,torch.zeros(x.shape[0],device=device)
-    
+
+        z = torch.cat([z0, z1], dim=1)
+        return z, torch.zeros(x.shape[0], device=device)
+
     def backward(self, z):
-        z0, z1 = z[:,:self.k], z[:,self.k:]
+    	"""
+    		Outputs:
+    		- output inverse coupling layer
+    		- log determinant of the Jacobian
+    	"""
+        z0, z1 = z[:, :self.k], z[:, self.k:]
 
         m = self.coupling(z0)
         x0 = z0
         x1 = z1-m
 
-        x = torch.cat([x0,x1], dim=1)
-        return x, torch.zeros(z.shape[0],device=device)
+        x = torch.cat([x0, x1], dim=1)
+        return x, torch.zeros(z.shape[0], device=device)
 
 
 class Scale(BaseNormalizingFlow):
     """
-        NICE
+    	Scaling layer
+
+    	Refs:
+    	- NICE: Non-linear independent components estimation
     """
-    def __init__(self, d):
+
+    def __init__(self, dim):
         super().__init__()
-        self.log_s = nn.Parameter(torch.randn(1, d, requires_grad=True))
-        
+        self.log_s = nn.Parameter(torch.randn(1, dim, requires_grad=True))
+
     def forward(self, x):
+    """
+    	Outputs:
+    	- z = x*s
+    	- log determinant of the jacobian: \sum_i \log s_i
+    """
         return torch.exp(self.log_s)*x, torch.sum(self.log_s, dim=1)
-    
+
     def backward(self, z):
+    """
+    	Outputs:
+    	- x = z/s
+    	- log determinant of the jacobian: -\sum_i \log s_i
+    """
         return torch.exp(-self.log_s)*z, -torch.sum(self.log_s, dim=1)
-        
-        
+
+
 class AffineCoupling(BaseNormalizingFlow):
     """
-        RealNVP
+    	Affine Coupling layer
+
+    	Refs:
+        - Density estimation using RealNVP
     """
-    def __init__(self, scaling, shifting, d):
+
+    def __init__(self, scaling, shifting, dim):
         super().__init__()
         self.scaling = scaling
         self.shifting = shifting
-        self.k = d//2
+        self.k = dim//2
 
     def forward(self, x):
-        x0, x1 = x[:,:self.k], x[:,self.k:]
+    	"""
+    		Outputs:
+    		- output coupling layer
+    		- log determinant of the Jacobian
+    	"""
+        x0, x1 = x[:, :self.k], x[:, self.k:]
 
         s = self.scaling(x0)
         t = self.shifting(x0)
         z0 = x0
         z1 = torch.exp(s)*x1+t
 
-        z = torch.cat([z0,z1], dim=1)
+        z = torch.cat([z0, z1], dim=1)
         return z, torch.sum(s, dim=1)
 
-
     def backward(self, z):
-        z0, z1 = z[:,:self.k], z[:,self.k:]
+    	"""
+    		Outputs:
+    		- output inverse coupling layer
+    		- log determinant of the Jacobian
+    	"""
+        z0, z1 = z[:, :self.k], z[:, self.k:]
 
         s = self.scaling(z0)
         t = self.shifting(z0)
         x0 = z0
         x1 = torch.exp(-s)*(z1-t)
-        
-        x = torch.cat([x0,x1], dim=1)
+
+        x = torch.cat([x0, x1], dim=1)
         return x, -torch.sum(s, dim=1)
-        
+
 
 class Reverse(BaseNormalizingFlow):
     """
-        Ref: https://github.com/acids-ircam/pytorch_flows/blob/master/flows_04.ipynb
+    	Reverse the indices
+
+        Refs:
+        - https://github.com/acids-ircam/pytorch_flows/blob/master/flows_04.ipynb
     """
-    def __init__(self, d):
+
+    def __init__(self, dim):
         super().__init__()
-        self.permute = torch.arange(d-1,-1,-1)
+        self.permute = torch.arange(dim-1, -1, -1)
         self.inverse = torch.argsort(self.permute)
-        
+
     def forward(self, x):
-        return x[:, self.permute], torch.zeros(x.size(0),device=device)
+        return x[:, self.permute], torch.zeros(x.size(0), device=device)
 
     def backward(self, z):
-        return z[:, self.inverse], torch.zeros(z.size(0),device=device)
+        return z[:, self.inverse], torch.zeros(z.size(0), device=device)
 
 
 class Shuffle(Reverse):
-    def __init__(self, d):
-        super().__init__(d)
-        self.permute = torch.randperm(d)
+	"""
+		Apply a random permutation of the indices
+	"""
+    def __init__(self, dim):
+        super().__init__(dim)
+        self.permute = torch.randperm(dim)
         self.inverse = torch.argsort(self.permute)
         
 
 class BatchNorm(BaseNormalizingFlow):
     """
-        Ref: https://github.com/acids-ircam/pytorch_flows/blob/master/flows_04.ipynb
-        and Masked Autoregressive Flows for Density Estimation / Density Estimation Using Real NVP
+        Refs: 
+        - https://github.com/acids-ircam/pytorch_flows/blob/master/flows_04.ipynb
+        - Masked Autoregressive Flows for Density Estimation 
+        - Density Estimation Using Real NVP
     """
-    def __init__(self, d, eps=1e-5, momentum=0.95):
+    def __init__(self, dim, eps=1e-5, momentum=0.95):
         super().__init__()
         self.eps = eps
         self.momentum = momentum ## To compute train set mean
-        self.train_mean = torch.zeros(d, device=device)
-        self.train_var = torch.ones(d, device=device)
+        self.train_mean = torch.zeros(dim, device=device)
+        self.train_var = torch.ones(dim, device=device)
 
-        self.gamma = nn.Parameter(torch.ones(d, requires_grad=True))
-        self.beta = nn.Parameter(torch.ones(d, requires_grad=True))
+        self.gamma = nn.Parameter(torch.ones(dim, requires_grad=True))
+        self.beta = nn.Parameter(torch.ones(dim, requires_grad=True))
 
     def forward(self, x):
         """
@@ -178,8 +229,12 @@ class BatchNorm(BaseNormalizingFlow):
         
 class LUInvertible(BaseNormalizingFlow):
     """
-        https://github.com/ikostrikov/pytorch-flows/blob/master/flows.py
-        https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
+    	Invertible 1x1 convolution (based on LU decomposition)
+    	
+    	Refs:
+    	- Glow: Generative flow with invertible 1×1 convolutions
+        - https://github.com/ikostrikov/pytorch-flows/blob/master/flows.py
+        - https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
     """
     def __init__(self, dim):
         super().__init__()
@@ -212,7 +267,8 @@ class LUInvertible(BaseNormalizingFlow):
 
 class PlanarFlow(BaseNormalizingFlow):
     """
-        Variational Inference with NF, https://arxiv.org/pdf/1505.05770.pdf
+    	Refs
+        - Variational Inference with Normalizing Flows, https://arxiv.org/pdf/1505.05770.pdf
     """
     def __init__(self, dim):
         super().__init__()
@@ -221,7 +277,7 @@ class PlanarFlow(BaseNormalizingFlow):
         self.b = nn.Parameter(torch.randn(1, 1, requires_grad=True))
 
     def forward(self, x):
-        ## enforce invertibility
+        # enforce invertibility
         wu = self.w@self.u.t()
         m_wu = -1+torch.log(1+torch.exp(wu))
         u_hat = self.u+(m_wu-wu)*self.w/torch.sum(self.w**2)
@@ -232,20 +288,21 @@ class PlanarFlow(BaseNormalizingFlow):
         return z, log_det[:,0]
 
     def backward(self, z):
-        ## can't compute it analytically
+        # can't compute it analytically
         return NotImplementedError
         
         
 class RadialFlow(BaseNormalizingFlow):
     """
-        Variational Inference with NF, https://arxiv.org/pdf/1505.05770.pdf
+    	Refs
+        - Variational Inference with Normalizing Flows, https://arxiv.org/pdf/1505.05770.pdf
     """
-    def __init__(self, d):
+    def __init__(self, dim):
         super().__init__()
-        self.d = d
+        self.d = dim
         self.log_alpha = nn.Parameter(torch.randn(1, requires_grad=True))
         self.beta = nn.Parameter(torch.randn(1, requires_grad=True))
-        self.z0 = nn.Parameter(torch.randn(d, requires_grad=True))   
+        self.z0 = nn.Parameter(torch.randn(dim, requires_grad=True))   
 
     def forward(self, x):
         r = torch.norm(x-self.z0,dim=-1,keepdim=True)
@@ -266,10 +323,11 @@ class RadialFlow(BaseNormalizingFlow):
     
 class AffineConstantFlow(BaseNormalizingFlow):
     """ 
-    Scales + Shifts the flow by (learned) constants per dimension.
-    In NICE paper there is a Scaling layer which is a special case of this where t is None
+    	Scales + Shifts the flow by (learned) constants per dimension.
+    	In NICE paper there is a Scaling layer which is a special case of this where t is None
 
-    https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
+		Refs:
+	    - https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
     """
     def __init__(self, dim, scale=True, shift=True):
         super().__init__()
@@ -293,11 +351,13 @@ class AffineConstantFlow(BaseNormalizingFlow):
 
 class ActNorm(AffineConstantFlow):
     """
-    Really an AffineConstantFlow but with a data-dependent initialization,
-    where on the very first batch we clever initialize the s,t so that the output
-    is unit gaussian. As described in Glow paper.
-
-    https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
+    	Really an AffineConstantFlow but with a data-dependent initialization,
+	    where on the very first batch we clever initialize the s,t so that the output
+	    is unit gaussian. As described in Glow paper.
+		
+		Refs:
+    	- https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
+    	- Glow: Generative flow with invertible 1×1 convolutions
     """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -317,9 +377,14 @@ class NormalizingFlows(BaseNormalizingFlow):
     """
         Composition of flows
         
-        (ref: https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py)
+        Refs: 
+        - https://github.com/karpathy/pytorch-normalizing-flows/blob/master/nflib/flows.py
     """
     def __init__(self, flows):
+    	"""
+    		Inputs:
+    		- flows: list of BaseNormalizingFlows objects
+    	"""
         super().__init__()
         self.flows = nn.ModuleList(flows)
         
